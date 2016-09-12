e1db6b8149e0fcb3604bd9318dd05ac6260eacb7
xqzh <xqzh@ideabinder.com>
2016-09-12 17:13:22 +0800

    Initial Commit

---
+++ 渐变色+进度条/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  渐变色+进度条
+//
+//  Created by xqzh on 16/9/12.
+//  Copyright © 2016年 xqzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
